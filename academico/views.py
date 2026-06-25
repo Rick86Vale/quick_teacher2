b82bc269cf15dc0ec557fc6932c7ab802a1734f9
Rick86Vale <ricardo86oliveira@gmail.com>
2026-06-25 12:10:36 -0300

    feat: implementado fluxo de reautenticação com senha para ações críticas e padronização do CRUD em views.py

--- academico/views.py
+++ academico/views.py
@@ -1,19 +1,31 @@
 # Path: academico/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
 from usuarios.views import eh_professor
 from .models import AreaConhecimento, Disciplina, Aula, Turma, Instituicao
-from .forms import TurmaForm, InstituicaoForm
+from .forms import TurmaForm, InstituicaoForm, DisciplinaForm
 
-# 1. Index
+# --- UTILS ---
+def verificar_senha_e_executar(request, acao_func, pk=None):
+    if request.method == 'POST':
+        senha = request.POST.get('password_confirm')
+        if request.user.check_password(senha):
+            return acao_func(request, pk)
+        else:
+            messages.error(request, "Senha incorreta. Ação cancelada.")
+    return render(request, 'academico/confirmar_senha.html', {'pk': pk})
+
+# --- 0. INDEX ---
 def index(request):
     areas = AreaConhecimento.objects.all()
     orfas = Disciplina.objects.filter(area__isnull=True)
     return render(request, 'academico/index.html', {'areas': areas, 'orfas': orfas})
 
-# 2. Instituições
+# --- 1. INSTITUIÇÕES ---
+# 1.1 Criar
 @login_required
-@user_passes_test(eh_professor, login_url='login')
+@user_passes_test(eh_professor)
 def criar_instituicao(request):
     if request.method == 'POST':
         form = InstituicaoForm(request.POST)
@@ -21,144 +33,129 @@ def criar_instituicao(request):
             inst = form.save(commit=False)
             inst.professor = request.user
             inst.save()
-            return redirect('dashboard_professor')
-    else:
-        form = InstituicaoForm()
-    return render(request, 'academico/criar_instituicao.html', {'form': form})
+            return redirect('listar_instituicoes')
+    return render(request, 'academico/criar_instituicao.html', {'form': InstituicaoForm()})
 
+# 1.2 Listar
 @login_required
-@user_passes_test(eh_professor)
 def listar_instituicoes(request):
     instituicoes = Instituicao.objects.filter(professor=request.user)
     return render(request, 'academico/listar_instituicoes.html', {'instituicoes': instituicoes})
 
+# 1.4 Editar
 @login_required
-@user_passes_test(eh_professor)
 def editar_instituicao(request, pk):
     inst = get_object_or_404(Instituicao, pk=pk, professor=request.user)
-    if request.method == 'POST':
-        form = InstituicaoForm(request.POST, instance=inst)
+    def acao_editar(req, p):
+        form = InstituicaoForm(req.POST, instance=inst)
         if form.is_valid():
             form.save()
             return redirect('listar_instituicoes')
-    else:
-        form = InstituicaoForm(instance=inst)
-    return render(request, 'academico/criar_instituicao.html', {'form': form})
+        return render(req, 'academico/criar_instituicao.html', {'form': form})
+    if request.method == 'POST': return verificar_senha_e_executar(request, acao_editar, pk)
+    return render(request, 'academico/criar_instituicao.html', {'form': InstituicaoForm(instance=inst)})
 
+# 1.5 Excluir
 @login_required
-@user_passes_test(eh_professor)
 def excluir_instituicao(request, pk):
-    inst = get_object_or_404(Instituicao, pk=pk, professor=request.user)
-    if request.method == 'POST':
-        inst.delete()
+    def acao_excluir(req, p):
+        get_object_or_404(Instituicao, pk=p, professor=req.user).delete()
         return redirect('listar_instituicoes')
-    return render(request, 'academico/confirmar_exclusao.html', {'item': inst})
+    return verificar_senha_e_executar(request, acao_excluir, pk)
 
-# 3. Turmas
+# --- 2. TURMAS ---
+# 2.1 Criar
+@login_required
+def criar_turma(request):
+    if request.method == 'POST':
+        form = TurmaForm(request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_turmas')
+    return render(request, 'academico/criar_turma.html', {'form': TurmaForm(user=request.user)})
+
+# 2.2 Listar
 @login_required
 def listar_turmas(request):
-    if request.user.is_superuser:
-        turmas = Turma.objects.all().order_by('instituicao__nome', 'nome')
-    else:
-        # Ordenamos por instituicao para o regroup do template funcionar
-        turmas = Turma.objects.filter(instituicao__professor=request.user).order_by('instituicao__nome', 'nome')
-    
+    turmas = Turma.objects.filter(instituicao__professor=request.user).order_by('instituicao__nome', 'nome')
     return render(request, 'academico/listar_turmas.html', {'turmas': turmas})
 
+# 2.3 Detalhes
 @login_required
 def detalhes_turma(request, pk):
     turma = get_object_or_404(Turma, pk=pk, instituicao__professor=request.user)
     return render(request, 'academico/detalhes_turma.html', {'turma': turma})
 
+# 2.4 Editar
 @login_required
-def criar_turma(request):
-    if not Instituicao.objects.filter(professor=request.user).exists():
-        return render(request, 'academico/sem_instituicao.html')
-    
-    if request.method == 'POST':
-        form = TurmaForm(request.POST, user=request.user)
-        if form.is_valid():
-            form.save() 
-            return redirect('listar_turmas')
-    else:
-        form = TurmaForm(user=request.user)
-    return render(request, 'academico/criar_turma.html', {'form': form})
-
-@login_required
-@user_passes_test(eh_professor)
 def editar_turma(request, pk):
     turma = get_object_or_404(Turma, pk=pk, instituicao__professor=request.user)
-    if request.method == 'POST':
-        form = TurmaForm(request.POST, instance=turma, user=request.user)
+    def acao_editar(req, p):
+        form = TurmaForm(req.POST, instance=turma, user=req.user)
         if form.is_valid():
             form.save()
             return redirect('listar_turmas')
-    else:
-        form = TurmaForm(instance=turma, user=request.user)
-    return render(request, 'academico/criar_turma.html', {'form': form})
+        return render(req, 'academico/criar_turma.html', {'form': form})
+    if request.method == 'POST': return verificar_senha_e_executar(request, acao_editar, pk)
+    return render(request, 'academico/criar_turma.html', {'form': TurmaForm(instance=turma, user=request.user)})
 
+# 2.5 Excluir
 @login_required
-@user_passes_test(eh_professor)
 def excluir_turma(request, pk):
-    turma = get_object_or_404(Turma, pk=pk, instituicao__professor=request.user)
-    if request.method == 'POST':
-        turma.delete()
+    def acao_excluir(req, p):
+        get_object_or_404(Turma, pk=p, instituicao__professor=req.user).delete()
         return redirect('listar_turmas')
-    return render(request, 'academico/confirmar_exclusao.html', {'item': turma})
+    return verificar_senha_e_executar(request, acao_excluir, pk)
 
-# 4. Disciplinas (CRUD Completo)
+# --- 3. DISCIPLINAS ---
+# 3.1 Criar
 @login_required
-@user_passes_test(eh_professor)
 def criar_disciplina(request):
     if request.method == 'POST':
         form = DisciplinaForm(request.POST)
         if form.is_valid():
-            disciplina = form.save(commit=False)
-            disciplina.professor = request.user
-            disciplina.save()
+            d = form.save(commit=False)
+            d.professor = request.user
+            d.save()
             return redirect('listar_disciplinas')
-    else:
-        form = DisciplinaForm()
-    return render(request, 'academico/criar_disciplina.html', {'form': form})
+    return render(request, 'academico/criar_disciplina.html', {'form': DisciplinaForm()})
 
+# 3.2 Listar
 @login_required
-@user_passes_test(eh_professor)
 def listar_disciplinas(request):
-    if request.user.is_superuser:
-        disciplinas = Disciplina.objects.all()
-    else:
-        disciplinas = Disciplina.objects.filter(professor=request.user)
+    disciplinas = Disciplina.objects.filter(professor=request.user)
     return render(request, 'academico/lista_disciplinas.html', {'disciplinas': disciplinas})
 
+# 3.4 Editar
 @login_required
-@user_passes_test(eh_professor)
 def editar_disciplina(request, pk):
-    disciplina = get_object_or_404(Disciplina, pk=pk, professor=request.user)
-    if request.method == 'POST':
-        form = DisciplinaForm(request.POST, instance=disciplina)
+    disc = get_object_or_404(Disciplina, pk=pk, professor=request.user)
+    def acao_editar(req, p):
+        form = DisciplinaForm(req.POST, instance=disc)
         if form.is_valid():
             form.save()
             return redirect('listar_disciplinas')
-    else:
-        form = DisciplinaForm(instance=disciplina)
-    return render(request, 'academico/criar_disciplina.html', {'form': form})
+        return render(req, 'academico/criar_disciplina.html', {'form': form})
+    if request.method == 'POST': return verificar_senha_e_executar(request, acao_editar, pk)
+    return render(request, 'academico/criar_disciplina.html', {'form': DisciplinaForm(instance=disc)})
 
+# 3.5 Excluir
 @login_required
-@user_passes_test(eh_professor)
 def excluir_disciplina(request, pk):
-    disciplina = get_object_or_404(Disciplina, pk=pk, professor=request.user)
-    if request.method == 'POST':
-        disciplina.delete()
+    def acao_excluir(req, p):
+        get_object_or_404(Disciplina, pk=p, professor=req.user).delete()
         return redirect('listar_disciplinas')
-    return render(request, 'academico/confirmar_exclusao.html', {'item': disciplina})
+    return verificar_senha_e_executar(request, acao_excluir, pk)
 
 
-# 5. Aulas
+
+# 4. Aulas
 @login_required
 def gerenciar_aulas(request, disciplina_id):
     disciplina = get_object_or_404(Disciplina, id=disciplina_id, professor=request.user)
     aulas = Aula.objects.filter(disciplina=disciplina)
     return render(request, 'academico/gerenciar_aulas.html', {
-        'disciplina': disciplina,
-        'aulas': aulas
+        'disciplina': disciplina, 'aulas': aulas
     })
+
+
